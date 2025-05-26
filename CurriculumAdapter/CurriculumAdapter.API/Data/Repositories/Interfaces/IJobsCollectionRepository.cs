e55ee678a1ffb1af88e87362a2bcb850dea0fd2f
jaofreire <joao.gabriel18872@gmail.com>
2025-05-25 23:55:45 -0300

    criando QDrantContext e adicionando Repository para buscar dados do QDrant

---
+++ CurriculumAdapter/CurriculumAdapter.API/Data/Repositories/Interfaces/IJobsCollectionRepository.cs
@@ -0,0 +1,9 @@
+﻿using Qdrant.Client.Grpc;
+
+namespace CurriculumAdapter.API.Data.Repositories.Interfaces
+{
+    public interface IJobsCollectionRepository
+    {
+        Task<IReadOnlyList<ScoredPoint>> SearchJobsBySimilarVectors(ReadOnlyMemory<float> vectors);
+    }
+}
---
+++ CurriculumAdapter/CurriculumAdapter.API/Data/Repositories/JobsCollectionRepository.cs
@@ -0,0 +1,18 @@
+﻿using CurriculumAdapter.API.Data.Context;
+using CurriculumAdapter.API.Data.Repositories.Interfaces;
+using Qdrant.Client.Grpc;
+
+namespace CurriculumAdapter.API.Data.Repositories
+{
+    public class JobsCollectionRepository(QdrantContext context) : IJobsCollectionRepository
+    {
+        private readonly QdrantContext _context = context;
+
+        public async Task<IReadOnlyList<ScoredPoint>> SearchJobsBySimilarVectors(ReadOnlyMemory<float> vectors)
+        {
+            var jobs = await _context.SearchBySimilarVectors("vagas", 20, vectors);
+
+            return jobs;
+        }
+    }
+}
