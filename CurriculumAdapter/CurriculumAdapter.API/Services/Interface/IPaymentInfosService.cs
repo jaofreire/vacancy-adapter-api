ef4f19ad03dce304efe9717b2377edc7fc830337
jaofreire <joao.gabriel18872@gmail.com>
2025-06-27 22:46:45 -0300

    implementando CRUD PaymentInfos

---
+++ CurriculumAdapter/CurriculumAdapter.API/Services/Interface/IPaymentInfosService.cs
@@ -0,0 +1,12 @@
+﻿using CurriculumAdapter.API.Models;
+using CurriculumAdapter.API.Response;
+
+namespace CurriculumAdapter.API.Services.Interface
+{
+    public interface IPaymentInfosService
+    {
+        Task<APIResponse<PaymentInfosModel>> GetByUserId(Guid userId);
+        Task<APIResponse<PaymentInfosModel>> Update(Guid id, PaymentInfosModel model);
+        Task<APIResponse<PaymentInfosModel>> Remove(Guid id);
+    }
+}
---
+++ CurriculumAdapter/CurriculumAdapter.API/Services/PaymentInfosService.cs
@@ -0,0 +1,39 @@
+﻿using CurriculumAdapter.API.Data.Repositories.Interfaces;
+using CurriculumAdapter.API.Models;
+using CurriculumAdapter.API.Response;
+using CurriculumAdapter.API.Services.Interface;
+
+namespace CurriculumAdapter.API.Services
+{
+    public class PaymentInfosService(IUnitOfWork unitOfWork) : IPaymentInfosService
+    {
+        private readonly IUnitOfWork _unitOfWork = unitOfWork;
+
+        public async Task<APIResponse<PaymentInfosModel>> GetByUserId(Guid userId)
+        {
+            var paymentInfos = await _unitOfWork.PaymentInfosRepository.Get(x => x.UserId == userId);
+
+            return new APIResponse<PaymentInfosModel>(true, 200, "Dados de pagamento listado com sucesso", null, paymentInfos);
+        }
+
+        public async Task<APIResponse<PaymentInfosModel>> Remove(Guid id)
+        {
+            var paymentInfo = await _unitOfWork.PaymentInfosRepository.GetById(id);
+
+            if(paymentInfo is null)
+                return new APIResponse<PaymentInfosModel>(false, 404, "Dados de pagemento não encontrado");
+
+            await _unitOfWork.BeginTransaction();
+
+            _unitOfWork.PaymentInfosRepository.Delete(paymentInfo);
+            await _unitOfWork.Commit();
+
+            return new APIResponse<PaymentInfosModel>(true, 200, "Dados de pagemento removidos com sucesso");
+        }
+
+        public async Task<APIResponse<PaymentInfosModel>> Update(Guid id, PaymentInfosModel model)
+        {
+            return new APIResponse<PaymentInfosModel>(true, 200, "Endpoint em implementação");
+        }
+    }
+}
